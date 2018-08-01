bae27d40a6dffd639a919c46f6cbad0a706c6e12
joehecn <leanbrown@live.cn>
2018-08-01 17:15:29 +0800

    c8

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -4,3 +4,21 @@
     <router-view />
   </div>
 </template>
+
+<style>
+html, body {
+  height: 100%;
+}
+
+html, body, div {
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+<style scoped>
+#app {
+  height: 100%;
+}
+</style>
+
--- src/renderer/views/Login.vue
+++ src/renderer/views/Login.vue
@@ -1,3 +1,46 @@
+
 <template>
-  <div>Login</div>
+  <div id="login">
+    <div id="card">
+      <div id="code-img">
+        <img src="">
+      </div>
+      <p id="msg">使用手机微信扫码登录</p>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+#login {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: url(../assets/login-bg.jpg) no-repeat 50%;
+  background-size: cover;
+}
+
+#card {
+  width: 300px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  background: white;
+}
+
+#code-img {
+  width: 270px;
+  height: 270px;
+  margin: 20px auto 10px;
+}
+
+#code-img img {
+  width: 270px;
+  height: 270px;
+}
+
+#msg {
+  text-align: center;
+}
+</style>
+
